1d81dfb0f56ab6ad17d255bd27dd03640a9f1d9f
AVG <glazyrinav1986@yandex.ru>
2026-04-20 15:18:23 +0300

    feat(Jest): Тесты Order-Info

--- src/__tests__/order-info.test.ts
+++ src/__tests__/order-info.test.ts
@@ -1,13 +1,69 @@
+import orderInfoSlice, {
+  findOrderById
+} from '../services/slice/order-info/order-info-slice';
+
+const initialState = {
+  order: null,
+  isLoading: false,
+  error: null
+};
+
+const order = {
+  _id: '1',
+  status: 'status 1',
+  name: 'name 1',
+  createdAt: 'now',
+  updatedAt: 'now',
+  number: 1,
+  ingredients: ['ingr1', 'ingr2']
+};
+
 describe('Проверка слайса Order-Info', () => {
   describe('Проверка получения информации о заказе c Fullfiled', () => {
-    test('Проверка findOrderById.fulfilled', () => {});
+    test('Проверка findOrderById.fulfilled', () => {
+      const actualState = orderInfoSlice(
+        { ...initialState, isLoading: true },
+        findOrderById.fulfilled({ orders: [order], success: true }, '', 1)
+      );
+
+      const expectedState = {
+        order: order,
+        isLoading: false,
+        error: null
+      };
+      expect(actualState).toEqual(expectedState);
+    });
   });
   describe('Проверка получения информации о заказе c Pending', () => {
-    test('Проверка findOrderById.pending', () => {});
+    test('Проверка findOrderById.pending', () => {
+      const actualState = orderInfoSlice(
+        { ...initialState },
+        findOrderById.pending('', 1)
+      );
+
+      const expectedState = {
+        order: null,
+        isLoading: true,
+        error: null
+      };
+      expect(actualState).toEqual(expectedState);
+    });
   });
   describe('Проверка получения информации о заказе c Rejected', () => {
     test('Проверка findOrderById.rejected', () => {
       const error = new Error('Ошибка');
+
+      const actualState = orderInfoSlice(
+        { ...initialState, isLoading: true },
+        findOrderById.rejected(error, '', 1)
+      );
+
+      const expectedState = {
+        order: null,
+        isLoading: false,
+        error: 'Ошибка'
+      };
+      expect(actualState).toEqual(expectedState);
     });
   });
 });
